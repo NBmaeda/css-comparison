be207c7741c031a64e671cd7d57c61f0710eff9d
dam <m.maeda@newbees.jp>
2023-01-11 11:09:15 +0900

    feat:ButtonCSSinJS.tsxのshadowColorの指定を変更。

--- src/components/ButtonCSSinJS.tsx
+++ src/components/ButtonCSSinJS.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-const shadowColor = "";
+const shadowColor = "#000";
 const Button = styled.button`
   box-shadow: 0 0 ${shadowColor}, 0 0 ${shadowColor},
     0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
